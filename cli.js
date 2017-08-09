2a8c4b39bf29fd23d86310da33a2da64084c0a25
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2017-08-10 00:37:08 +0100

    added create task

--- cli.js
+++ cli.js
@@ -11,23 +11,22 @@ const cli = meow(`
    $ drone install                          # downloads the packages
    $ drone add <username/repo> <module>     # adds a dependency to a module
    $ drone info <username/repo>             # retrieves the info for this package
-   $ drone create <todo>                    # creates a new one if it's not in the server yet
+   $ drone create                           # creates a new one if it's not in the server yet
    
-   # Multiline
-   $ drone create square/picasso  \
-          --homepage http://square.github.io/picasso \
-          --description "image downloading and caching library" \
-          --dependency com.squareup.picasso:picasso \
-          --server jcenter
-
  Examples
-   $ drone info square/picasso to module
+   $ drone info square/picasso
    $ drone add reactivex/rxjava to module
    $ drone add square/retrofit to module
    $ drone add support/design to module
    $ drone add google/gson to module
    
    $ drone create square/picasso --dependency com.squareup.picasso:picasso
+   # Multiline
+   $ drone create square/picasso  \\
+          --homepage http://square.github.io/picasso \\
+          --description "image downloading and caching library" \\
+          --dependency com.squareup.picasso:picasso \\
+          --server jcenter
    `,
 {
   alias: {
