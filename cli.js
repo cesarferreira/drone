3c7b37e7979017b5c8cc92a55ea7c4405070f33e
cesarferreira <cesar.manuel.ferreira@gmail.com>
2017-08-16 23:53:07 +0100

    added search

--- cli.js
+++ cli.js
@@ -2,45 +2,43 @@
 'use strict';
 
 const meow = require('meow');
-const core = require('./lib/router');
+const router = require('./lib/router');
 
 const cli = meow(`
- Usage
-   
+Usage
+
    $ drone <command> <params>
 
    $ drone install                          # downloads the packages
    $ drone add <username/repo> <module>     # adds a dependency to a module
    $ drone info <username/repo>             # retrieves the info for this package
    $ drone create                           # creates a new one if it's not in the server yet
+   $ drone search <library>                 # searches for the 'library' in the hive
    
  Examples
+
+   $ drone info square/picasso              # Shows the info for square/picasso
    
-   $ drone info square/picasso
-   $ drone add reactivex/rxjava to module
-   $ drone add support/design to module
-   $ drone add google/gson to module
+   $ drone add reactivex/rxjava app         # Adds RxJava to the 'app' module
+   $ drone add square/retrofit app          # Adds retrofit to the 'app' module
+   $ drone add support/design app           # Adds design support to the 'app' module
+   $ drone add google/gson app              # Adds GSON to the 'app' module
    
-   $ drone create square/picasso --dependency com.squareup.picasso:picasso
+   $ drone create                           # takes you throught the wizard
    
-   # Multiline
-   $ drone create square/picasso  \\
-          --homepage http://square.github.io/picasso \\
-          --description "image downloading and caching library" \\
-          --dependency com.squareup.picasso:picasso \\
-          --server jcenter
-   `,
+   $ drone search rxjava                    # Searches the hive for 'rxjava'
+`,
 {
   alias: {
-    h: 'homepage', // Optional
-    d: 'description', // Optional
-    s: 'server', // Optional
-    d: 'dependency'
+    // h: 'homepage', // Optional
+    // d: 'description', // Optional
+    // s: 'server', // Optional
+    // d: 'dependency'
   }
 });
 
 if (cli.input.length > 0 ) {
-	core.init(cli.input, cli.flags);
+	router.init(cli.input, cli.flags);
 } else {
 	cli.showHelp(2);
 }
